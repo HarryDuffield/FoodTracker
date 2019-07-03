7fb854e4d59badbe43af9f7d91b64b3f4f02cc87
brad2307 <brad1092@live.co.uk>
2019-07-03 14:25:36 +0100

    e

---
+++ FoodTrackerBradley/FoodTracker/FoodTracker/Recipe.swift
@@ -0,0 +1,23 @@
+//
+//  Recipe.swift
+//  FoodTracker
+//
+//  Created by  on 03/07/2019.
+//  Copyright © 2019 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class Recipe: NSObject {
+    
+    //MARK: Properties
+    /*var quantity: Float = 0
+    var unit: String = ""
+    var ingredient: String = ""*/
+    let quantity = [Float]()
+    let unit = [String]()
+    let ingredient = [String]()
+    
+    //MARK: Types
+    
+}
