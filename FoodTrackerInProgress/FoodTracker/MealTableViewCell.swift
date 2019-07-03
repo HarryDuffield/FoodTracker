0e20944287be0e561440c08b58be2ba8a996deab
HarryDuffield <15duffieldh@utcsheffield.org.uk>
2019-07-03 14:10:10 +0100

    this is the file to work on

---
+++ FoodTrackerInProgress/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var cellDescription: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
