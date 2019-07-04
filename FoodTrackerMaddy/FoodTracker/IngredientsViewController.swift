68ce5724fec266a2793c9b55d44079f6bd725807
Maddy <maddy.flynn@icloud.com>
2019-07-04 15:54:10 +0100

    recipe links to addition pages

---
+++ FoodTrackerMaddy/FoodTracker/IngredientsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IngredientsViewController.swift
+//  FoodTracker
+//
+//  Created by  on 04/07/2019.
+//  Copyright © 2019 Maddy Flynn. All rights reserved.
+//
+
+import UIKit
+
+class IngredientsViewController: UIViewController {
+
+    @IBOutlet weak var addIngredientTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var addInstructionTextField: UITextField!
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
